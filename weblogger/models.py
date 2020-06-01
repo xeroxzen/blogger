8d28c224b0c12447d11b569a0b8f56084301fb7d
Andile Jaden aka Google Jr <andilembele020@gmail.com>
2020-06-01 09:11:47 +0200

    Added Tweepy

--- weblogger/models.py
+++ weblogger/models.py
@@ -122,7 +122,18 @@ class NewsLetter(models.Model):
     def get_absolute_url(self):
         return reverse("NewsLetter_detail", kwargs={"pk": self.pk})
             
-    
+
+class Tweet(models.Model):
+    tweet_id = models.CharField(max_length=250, null=True, blank=True)
+    tweet_text = models.TextField()
+    published_date = models.DateTimeField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.tweet_text
+        
+    class Meta:
+        verbose_name_plural= 'Tweets'    
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
